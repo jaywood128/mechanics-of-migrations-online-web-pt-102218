ebec054f0752727916dd7152ebcfa36e5a34513c
Johnathon Wood <jaywood128@gmail.com>
2019-01-23 21:23:05 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,6 +1,7 @@
 class CreateArtists < ActiveRecord::Migration
   def change
-    create_table :artists do |t|
+
+      create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
